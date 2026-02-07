be4509a05793506198a0595567da22470c04d136
Christo Santhosh <christosanthosh5@gmail.com>
2026-02-07 14:53:18 +0530

    Question No. 5
    Uploaded the programs

---
+++ assi2.py
@@ -0,0 +1,5 @@
+s = 0
+print("Enter the numbers: ")
+for i in range(10):
+    s += int(input())
+print("The average is: ", s/10)
---
+++ assi3.py
@@ -0,0 +1,8 @@
+n = int(input("Enter size n: "))
+lis = []
+
+print("Enter the numbers: ")
+for i in range(n):
+    lis.append(int(input()))
+
+print("The Sum is : ",sum(lis))
